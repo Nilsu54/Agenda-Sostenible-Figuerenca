41a13de429f29ecb36711408994cc285473124f7
Nil Suñer Xarles <nsuner@cendrassos.net>
2024-11-12 19:40:33 +0100

    correciones

--- src/controllers/storeUserController.php
+++ src/controllers/storeUserController.php
@@ -1,32 +1,32 @@
 <?php
 
-// Controller to store a new user in the database.
+// Controlador para almacenar un nuevo usuario en la base de datos.
 function storeUserController($request, $response, $container) {
     
-    // Get the user model from the dependency container.
+    // Obtiene el modelo de usuarios desde el contenedor de dependencias.
     $users = $container->Users();
 
-    // Retrieve registration form data sent via POST.
+    // Recupera los datos del formulario de registro enviados por POST.
     $name = $request->get(INPUT_POST, "name");
     $surname = $request->get(INPUT_POST, "surname");
     $username = $request->get(INPUT_POST, "username");
     $email = $request->get(INPUT_POST, "email");
     
-    // Encrypt the password using the BCRYPT algorithm.
+    // Encripta la contraseña utilizando el algoritmo BCRYPT.
     $password = password_hash($request->get(INPUT_POST, "password"), PASSWORD_BCRYPT);
 
-    // Variable for the image (not used here, but could be extended to support image uploads).
+    // Variable para la imagen (en este caso no se está utilizando, pero se podría extender para soportar carga de imágenes).
     $img = "";
 
-    // For debugging: Print the value of the "username" variable (this should probably be removed in production).
+    // Para depuración: Imprime el valor de la variable "username" (probablemente esto debería ser eliminado en producción).
     print_r("username");
 
-    // Call the method to add a new user to the database with the received data.
+    // Llama al método para agregar un nuevo usuario en la base de datos con los datos recibidos.
     $users->add($username, $password, $email, $img, $name, $surname);
 
-    // Redirect the user to the login page with an "ok" parameter to indicate successful registration.
+    // Redirige al usuario a la página de login con un parámetro "ok" para indicar que el registro fue exitoso.
     $response->redirect("location:index.php?r=login&ok=1");
 
-    // Return the final response.
+    // Devuelve la respuesta final.
     return $response;
 }
