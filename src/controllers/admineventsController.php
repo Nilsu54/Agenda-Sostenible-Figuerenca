a85b353271975c9940c1f502e2b567fd4494f23c
Nil Suñer Xarles <nsuner@cendrassos.net>
2024-11-13 17:01:07 +0100

    correccions

--- src/controllers/admineventsController.php
+++ src/controllers/admineventsController.php
@@ -1,5 +1,15 @@
 <?php
-function adminEventsController($request,$response,$container){
-    $response->setTemplate("adminEvents.php");
+function admineventsController($request,$response,$container){
+
+    // Obtiene la instancia del modelo de usuarios desde el contenedor de dependencias.
+    $eventos = $container->events();
+
+    // Llama al método list del modelo para obtener la lista de usuarios.
+    $events = $eventos->list();
+
+    // Añade la lista de usuarios al objeto de respuesta para su uso en la vista.
+    $response->set("events", $events);
+
+    $response->setTemplate("adminesdeveniments.php");
     return $response;
 }
